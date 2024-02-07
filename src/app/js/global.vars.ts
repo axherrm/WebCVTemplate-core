f21f6d193b111b333eda6e4bb70c00e628f6c23c
axherrm <axel-herrmann@gmx.net>
2024-02-08 00:27:48 +0100

    adjust footer

--- src/app/js/global.vars.ts
+++ src/app/js/global.vars.ts
@@ -2,4 +2,4 @@ import packageJson from '../../../package.json';
 
 export const appVersion: string = packageJson.version;
 export const appName: string = "WebCVTemplate";
-export const githubURL: string = "https://github.com/axherrm/CV";
+export const githubURL: string = "https://github.com/axherrm/WebCVTemplate";
--- src/app/sections/footer-section/footer-section.component.ts
+++ src/app/sections/footer-section/footer-section.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import {appVersion, githubURL} from "../../js/global.vars";
+import {Component, ElementRef} from '@angular/core';
+import {appName, appVersion, githubURL} from "../../js/global.vars";
 import {CustomButtonComponent} from "../../components/custom-button/custom-button.component";
+import gsap from "gsap";
 
 @Component({
   selector: 'footer-section',
@@ -15,7 +16,33 @@ export class FooterSectionComponent {
 
   protected readonly appVersion = appVersion;
   protected readonly githubURL = githubURL;
+  protected readonly appName = appName;
 
   currentYear: string = '' + new Date().getFullYear();
+
+  constructor(readonly host: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.addAnimation();
+  }
+
+  addAnimation() {
+    const tl: gsap.core.Timeline = gsap.timeline({
+      scrollTrigger: {
+        start: "bottom 140%",
+        trigger: "body",
+        end: "bottom bottom",
+        scrub: true
+      }
+    });
+    tl
+      .fromTo(this.host.nativeElement, {
+        display: "none",
+        opacity: "0"
+      },{
+        display: "block",
+        opacity: 1,
+      });
+  }
 
 }
